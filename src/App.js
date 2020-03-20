fc8794c90065819419e7596ff085632d7ccc2b1b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-20 15:43:28 -0300

    :sparkles: usando Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [techs, setTechs] = useState(['ReactJS', 'React Native']);
+  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTechs([...techs, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storedTechs = localStorage.getItem('techs');
+
+    if (storedTechs) {
+      setTechs(JSON.parse(storedTechs));
+    }
+
+    // *** ComponentWillUnmount ***
+    // return () => {
+    //   document.removeEventListener();
+    // };
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
 
   return (
     <>
