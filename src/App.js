dfc703f31a4cecf2e86ec57d77f6e34e42599fb1
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-20 15:50:04 -0300

    :sparkles: usando Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [techs, setTechs] = useState([]);
@@ -22,6 +22,8 @@ function App() {
     // };
   }, []);
 
+  const techsSize = useMemo(() => techs.length, [techs]);
+
   useEffect(() => {
     localStorage.setItem('techs', JSON.stringify(techs));
   }, [techs]);
@@ -33,6 +35,12 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>
+        Você tem
+        {techsSize}
+        tecnologias
+      </strong>
+      <br />
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
